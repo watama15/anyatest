dba1693051cb9876de123f765387ccef612229a0
watama15 <watamasarunge@gmail.com>
2022-09-28 16:06:16 +0700

    anyatest

---
+++ plugins/info-script.js
@@ -0,0 +1,25 @@
+//Credits Jangan Dihapus
+//Thanks For KannaChan & Papah-Chan
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*Mau Nyumri SCeh Ya Bang? :'v*\n\nMending Sewa Bot\nwa.me/628136444859`
+let wibu = `https://api.zacros.my.id/randomimg/waifu` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/k0nc1t.store",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/p/Cfn8ivgvbqs/?igshid=YmMyMTA2M2Y=", 
+    title: 'Anya-Bot MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
